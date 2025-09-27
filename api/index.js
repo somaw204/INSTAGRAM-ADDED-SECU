6cc0eabe774a81e89df54dc4a3e7d836f429bd9b
somaw204 <tsnp49767@gmail.com>
2025-09-27 07:34:54 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.status(200).send('#PWD_INSTAGRAM_BROWSER:10:1758721587:AVdQAGMluSHXlSC/CN4ZeBrrDdvEgoG1bLoufPWx/o9xZGxMuWDCzoHzO5aFL6ea6R/xMu3t5IXIXKtIpPJDuk+DFEHOUh2fgMI2JUSnwHTI1ZBBtuMERc3Lo3vX6dmImJDQTxKyTTh9bE/bqyUL+1JlhlBI5Llb');
+  res.status(200).send('https://accountscenter.instagram.com/password_and_security/two_factor/?theme=dark');
 }
